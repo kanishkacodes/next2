b59a851ff9a760d9291fd06b8a0e647f6ed7a151
Kanishka Kumar <kumarkanishka2111@gmail.com>
2023-07-25 08:11:51 +0530

    day-3

--- spotify-clone/components/Button.tsx
+++ spotify-clone/components/Button.tsx
@@ -1,10 +1,11 @@
 import { forwardRef } from "react";
+import {twMerge} from "tailwind-merge";
 
 interface ButtonProps 
 extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 
-
+ 
 
 const Button = forwardRef< HTMLButtonElement, ButtonProps>(({
       className,
@@ -15,8 +16,33 @@ const Button = forwardRef< HTMLButtonElement, ButtonProps>(({
     }
     , ref) => {
         return (
-            <button>
+            <button 
+            type = {type}
+            className={twMerge(`
+            w-full
+            rounded-full
+            bg-green-500
+            border
+            border-transparent
+            px-3
+            py-3
+            disabled:cursor-not-allowed
+            disabled:opacity-50
+            text-black
+            font-bold
+            hover:opacity-75
+            transition
 
+            `
+            , className
+            )}
+            disabled={disabled}
+            ref={ref}
+            {...props}
+
+            >
+                {children}
+                 
             </button>
         )
     }
--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -111,18 +111,28 @@ const Header: React.FC<HeaderProps> = ({
                 </div>
                 <div className=" flex justify-between items-center gap-x-4">
                     <>
-                    <div>
-                        <Button>
+                     <div>
+                        <Button 
+                         onClick={()=>{}}
+                        className=" bg-transparent text-neutral-300 font-medium">
                             Sign Up
                         </Button>
-                    </div>
+                     </div>
+                     <div>
+                        <Button 
+                        onClick={()=>{}}
+                        className=" bg-white px-6 py-2">
+                            Log in
+                        </Button>
+                     </div>
                     </>
 
 
                 </div>
             </div>
+            {children}
             
         </div>
     );}
-
+ 
 export default Header;
