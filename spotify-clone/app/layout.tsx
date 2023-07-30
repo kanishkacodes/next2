9a7299b0ec3fd518ffa7c61525a7937f66211c8c
Kanishka Kumar <kumarkanishka2111@gmail.com>
2023-07-31 03:37:07 +0530

    Day-7

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -7,19 +7,26 @@ import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 
 import  TosterProvider  from '@/providers/TosterProvider'
+import getSongsByUserId from '@/actions/getSongsByUserId'
 
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Spotify Clone',
   description: 'Listen to music!',
-}
+};
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+
+  const userSongs = await getSongsByUserId();  
+
   return (
     <html lang="en">
       <body className={font.className}>
@@ -33,7 +40,7 @@ export default function RootLayout({
       <SupabaseProvider>
           <UserProvider>
             <ModalProvider/>
-            <Sidebar>
+            <Sidebar songs = {userSongs}>
               {children}
              </Sidebar>
          </UserProvider>
--- spotify-clone/components/Library.tsx
+++ spotify-clone/components/Library.tsx
@@ -5,9 +5,17 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
+
+interface LibraryProps {
+    songs: Song[];
+  }
 
 
-const Library = () => {
+  const Library: React.FC<LibraryProps> = ({
+    songs
+  }) => {
 
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
@@ -39,7 +47,21 @@ const Library = () => {
                 className=" text-neutral-400 cursor-pointer hover:text-white transition"
                 />
             </div>
-            <div className=" flex flex-col gap-y-2 mt-4 px-3">List of Songs!</div>
+            <div className=" flex flex-col gap-y-2 mt-4 px-3">
+
+
+
+            {songs.map((item) => (
+
+                
+          <MediaItem 
+            // onClick={(id: string) => onPlay(id)} 
+            key={item.id} 
+            data={item}
+          />
+        ))}
+
+            </div>
         </div> 
     );
 }
